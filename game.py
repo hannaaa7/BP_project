edc70b106c90b9cd9b691f03842391d38af962c1
hannaaa7 <157493932+hannaaa7@users.noreply.github.com>
2024-01-27 17:23:16 +0330

    Add files via upload

---
+++ game.py
@@ -0,0 +1,33 @@
+import randomm
+#کتابخانه ریاضی را فراخوانی میکنیم.
+n = int(input())
+#تعداد دست هایی که بازی باید انجام شود را از کاربر دریافت میکنیم
+player = []
+i = 0
+while i < 20:
+    player.append(i)
+    i += 1
+money = []
+#یک لیست خالی از پول بازیکنان که 20 نفر هستنذ میسازیم
+while len(money) < 20:
+    money.append(25)
+# یک حلقه ایجاد میکنیم و به تک تک بازیکن ها که 20 نفر هستن نفری 25 دلار میدهیم
+i = 0
+seed = "h"
+while i < n:
+    j = 0
+    while j < len(player):
+        daryaft = randomm.random_int(n=1, a=0, b=len(player), xn= seed)
+        daryaft = daryaft[0]
+        seed += "b"
+#برای هربار تغییر مقدار اولیه و داشتن یک عدد رندوم متفاوت، مقدار ثابت را تغییر میدهیم
+        money[player[j]-1] -= 1
+        money[player[daryaft]-1] += 1
+# از بازیکنی که نوبتش دشه یه واحد کم میکنیم و به بازیکنی که رندوم انتخاب شده یه واحد اضافه میکنیم
+        if money[player[j]-1] == 0:
+            player.remove(player[j])
+# اگر پول یکی از بازیکنان صفر شود از لیست انتخاب رندوم حذف میکینم
+        j += 1
+    i += 1
+
+print(money, len(player))
